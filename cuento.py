b202f5031868a993b5a8940acf63bb893bd3d41d
haarii238 <a01800524@tec.mx>
2024-10-30 09:46:56 -0600

    segundo commit

---
+++ cuento.py
@@ -0,0 +1,106 @@
+print ("Hola este es un nuevo cambio")
+nombre= input ("primero dime tu nombre")
+
+print ("De acuerdo"+ nombre+ "list@ para aterrarte")
+print ("Para saber que nivel de terror te puedo  ")
+
+print ("Presiona el número 1 si tienes +18")
+print ("Presiona el número dos si tienes -18")
+print ("Presiona el número 3 si quieres salir")
+
+opción= int (input ("¿Qué opción escoges?"))
+
+if opción == "1":
+    print ("¿Cuánta intensidad de terror quieres?")
+    print ("El número 1 para leve")
+    print ("El número 2 para intermedio")
+    print ("El número 3 para fuerte")
+intensidad=input ("¿Qué escoges?")
+
+if intensidad== "1":
+    print("Escogiste intensidad leve")
+
+    mesa= "mesa"
+    cuchillo= "silla"
+    televisión= "televisión"
+
+    maldición= input ("Escoge y escribe el nombre de alguno de estos objetos \n-mesa-\n-silla-\n-televisión-")
+
+    if maldición == "mesa"
+        print(" Tu objeto maldito es una mesa")
+        persona= input("Nombre de tu personaje principal:")
+
+    
+    elif maldición== "silla":
+        print ("Tu objeto maldito es un cuchillo")
+
+    elif maldición == "televisión":
+        print ("Tu objeto maldito es un dige")
+
+elif intensidad== 2:
+    print ("Escogiste intensidad intermedia")
+
+    cuchillo= "cuchillo"
+    dige= "dige"
+    muñeca= "muñeca"
+
+    ob_maldito=input("Escoge y escribe el nombre de alguno de estos objetos \n-cuchillo-\n-dige-\n-muñeca-")
+
+elif intensidad==3:
+    print("Escogiste intensidad fuerte") 
+
+if opción== "2":
+    print("¿Cuánta cantidad de terror quieres?")
+    print("El número 1 para leve")
+    print ("El número 2 para intermedio")
+    cantidad=input("¿Qué escoges?")
+
+if cantidad==1:
+     print("Escogiste intensidad leve")
+elif cantidad==2:
+    print ("Escogiste intensidad intermedia")
+
+print ("Vamos a crear un cuento juntos")
+nombre= input("Primero dime tu nombre")
+
+print ("De acuerdo"+ nombre+ "list@ para aterrarte?")
+print ("Para saber que nivel de terror te puedo brindar necesito saber tu edad")
+
+print("Presiona el número 1 si tienes +18")
+print("Presiona el número 2 si tienes -18")
+print("Presiona el número 3 si quieres salir")
+
+opción= int(input("¿Qué opcion escoges?"))
+
+if opción == "1":
+    print("¿Cuánta cantidad de terror quieres?")
+    print("El número 1 para leve")
+    print("El número 2 para intermedio")
+    print("El número 3 para fuerte")
+    intensidad=input("¿Qué escoges? ")
+
+if intensidad == 1:
+    print("Escogiste intensidad leve")
+        
+    mesa= "mesa"
+    silla= "silla"
+    televisión= "televisión"
+
+maldición=input("Escoge y escribe el nombre de alguno de estos objetos \n-mesa-\n-silla-\n-televisión-")
+
+if maldición == "mesa":
+            print("Tu objeto maldito es una mesa")
+            persona= input("Nombre de tu personaje principal: ")
+            persona_1_1= input ("Nombre de otro personaje: ")
+            print("Vamos por el cuento\n")
+            print("\nLa Mesa que Susurraba \nEn una antigua mansión, heredada de generación en generación, se encontraba una mesa de roble macizo. Era una pieza imponente, tallada con intrincados diseños que parecían cobrar vida a la luz de la luna. Los nuevos propietarios, una joven pareja " +persona, +persona_1_1 +"habían quedado fascinados por su belleza y decidieron colocarla en el comedor. \nAl principio, todo parecía normal. La mesa servía de apoyo para comidas familiares, reuniones con amigos y juegos de mesa. Sin embargo, con el paso de los días, comenzaron a notar extraños sucesos. Los cubiertos se movían solos, las sillas crujían sin razón aparente y, a veces, creían escuchar susurros provenientes de la madera. \nUna noche, mientras cenaban a la luz de las velas, la mujer sintió una presencia fría en la nuca. Al volverse, vio que una de las sillas se había desplazado unos centímetros, como si alguien se hubiera levantado de ella. Su marido, tratando de tranquilizarla, le aseguró que solo era el viento. \nLos susurros se hicieron cada vez más frecuentes. Parecían palabras ininteligibles, como si la mesa intentara contar una historia antigua. La pareja consultó a un experto en lo paranormal, quien les explicó que la mesa podría estar vinculada a un suceso trágico ocurrido en esa misma habitación. Tal vez, alguien había sido asesinado allí, y la mesa había absorbido la energía negativa del lugar. \nDecididos a poner fin a las perturbaciones, los jóvenes propietarios decidieron llevar a cabo un ritual de limpieza espiritual. Encendieron velas, quemaron hierbas aromáticas y recitaron antiguas plegarias. Al finalizar el ritual, la atmósfera de la habitación se sintió más ligera. Los susurros cesaron y la mesa pareció recuperar su calma.\nSin embargo, la pareja nunca se olvidó de la extraña experiencia. A veces, al pasar por el comedor, tenían la sensación de que la mesa los observaba con ojos antiguos y sabios. Y aunque los sucesos paranormales habían cesado, no podían evitar preguntarse qué secretos ocultaba esa vieja mesa de roble.")
+        
+    elif maldición == "silla":
+            print("Tu objeto maldito es un silla")
+            persona_1= input("Nombre de tu personaje principal: ")
+
+     elif maldición == "televisión":
+            print("Tu objeto maldito es un televisión")
+            persona_2= input("Nombre de tu personaje principal: ")
+               
+       
